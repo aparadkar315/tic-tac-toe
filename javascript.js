8c829ba74ef93afde274833a7930ad220c803a43
Abhishek Paradkar <aparadkar315@gmail.com>
2025-12-15 13:24:32 +0530

    Add display function and rename gameController function

--- javascript.js
+++ javascript.js
@@ -6,19 +6,22 @@ const gameBoard = (function(){
     for(let i = 0; i < rows; i++){
         board[i] = [];
         for(let j = 0; j < columns; j++){
-            board[i].push(cell());
+            board[i].push("");
         }
     }
     return {board};
 })();
 
 
-function cell(){
+/*function cell(){
     const value = "";
     return value;
-}
+}*/
 
-function gameController(){
+//factory function to store player and switch player
+//                   |
+//                   V
+function player(){
     const players = [
         {
             playerOneName : "Player1",
@@ -29,7 +32,16 @@ function gameController(){
             token : "O"
         }
     ];
+    return {players};
+}
 
+
+const playerObj = player();
+const players = playerObj.players;
+console.log(players);
+
+
+function playGame() {
 
     let activePlayer;
 
@@ -41,24 +53,105 @@ function gameController(){
             activePlayer = players[0];
         }
         return activePlayer;
-};
+    };
+
+    const getActivePlayer = () => {
+        return switchPlayerTurn();
+    };
+
+    const updateBoard = (x,y) => {
+        activePlayer = getActivePlayer();
+        
+        if(updatedBoard[x][y]===""){
+            updatedBoard[x][y] = activePlayer.token;
+            return updatedBoard;
+        }else{
+            updatedBoard[x][y] = updatedBoard[x][y];
+            return updatedBoard;
+        }
+    };
+
+    return {updateBoard,getActivePlayer};
     
-    return {switchPlayerTurn};
-}
-
-let activePlayer  = gameController();
-
-
-console.log(gameBoard);
-console.log(activePlayer.switchPlayerTurn());
-console.log(activePlayer.switchPlayerTurn());
-console.log(activePlayer.switchPlayerTurn());
-console.log(activePlayer.switchPlayerTurn());
-
-
-function displayController(){
 
 }
+
+    function display() {
+        const game = playGame();
+        
+
+        const getBoard = (x,y) => {
+            updatedBoard = game.updateBoard(x,y);
+            return updatedBoard;
+        }
+
+        const getResult = (updatedBoard) => {
+         if(updatedBoard[0][0] === updatedBoard[0][1] && updatedBoard[0][1] === updatedBoard[0][2] && updatedBoard[0][2]||
+            updatedBoard[1][0] === updatedBoard[1][1] && updatedBoard[1][1] === updatedBoard[1][2] && updatedBoard[1][2]||
+            updatedBoard[2][0] === updatedBoard[2][1] && updatedBoard[2][1] === updatedBoard[2][2] && updatedBoard[2][2]||
+            updatedBoard[0][0] === updatedBoard[1][0] && updatedBoard[1][0] === updatedBoard[2][0] && updatedBoard[2][0]|| 
+            updatedBoard[0][1] === updatedBoard[1][1] && updatedBoard[1][1] === updatedBoard[2][1] && updatedBoard[2][1]||
+            updatedBoard[0][2] === updatedBoard[1][2] && updatedBoard[1][2] === updatedBoard[2][2] && updatedBoard[2][2]||
+            updatedBoard[0][0] === updatedBoard[1][1] && updatedBoard[1][1] === updatedBoard[2][2] && updatedBoard[2][2]||
+            updatedBoard[0][2] === updatedBoard[1][1] && updatedBoard[1][1] === updatedBoard[2][0] && updatedBoard[2][0]){
+            if(game.getActivePlayer() === players[0]){
+                    return "Player2 wins!";
+                }else{
+                    return "Player1 wins!";
+                    }
+                }else{
+                    return "Next players turn";
+                } 
+        };
+        return {getBoard, getResult};
+    }
+     
+   
+
+   
+            
+       
+    
+
+let updatedBoard = gameBoard.board;
+
+let displayer = display();
+updatedBoard = displayer.getBoard(0,0);
+console.log(updatedBoard);
+console.log(displayer.getResult(updatedBoard));
+
+updatedBoard = displayer.getBoard(2,1);
+console.log(updatedBoard);
+console.log(displayer.getResult(updatedBoard));
+
+updatedBoard = displayer.getBoard(1,1);
+console.log(updatedBoard);
+console.log(displayer.getResult(updatedBoard));
+
+updatedBoard = displayer.getBoard(1,2);
+console.log(updatedBoard);
+console.log(displayer.getResult(updatedBoard)); 
+
+updatedBoard = displayer.getBoard(2,2);
+console.log(updatedBoard);
+console.log(displayer.getResult(updatedBoard));
+
+updatedBoard = displayer.getBoard(0,1);
+console.log(updatedBoard);
+console.log(displayer.getResult(updatedBoard));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
